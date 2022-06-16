835e47c1814ca5411059b76e5b7e830b4839d0f9
Your Name <fedi.benslama@esprit.tn>
2022-06-16 16:51:29 +0100

    Part 2 done

---
+++ src/CardList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "./Card";
+
+
+const CardList = ({ robots }) => {
+
+  const CardComponant = robots.map((user, i) => {
+    return (
+      <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />)
+  })
+
+  return (
+    <div>
+      {CardComponant}
+    </div>
+
+  )
+}
+export default CardList
